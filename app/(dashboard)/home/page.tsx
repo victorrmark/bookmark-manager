bc8d69254dae3e96e3ad58ac07c9f6b5fa46e7fa
victorrmark <innovimark@gmail.com>
2026-01-30 15:35:36 +0100

    added sort, filter and search functionality to bookmark list

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,15 +1,52 @@
 'use client'
 import BookmarkCard from "@/components/layout/bookmarks-card";
 import { useBookmark } from "@/hooks/useBookmark";
+import { useMemo } from "react";
+import { useBookmarkContext } from "../BookmarkContext";
 
 
 export default function Home() {
-  const { data: bookmarks } = useBookmark();
-  console.log("Bookmarks Data:", bookmarks);
+  const { data: bookmarks = [] } = useBookmark();
+  const { selectedId, searchQuery, sortBy } = useBookmarkContext();
+
+  // console.log("Bookmarks Data:", bookmarks);
+  const visibleBookmarks = useMemo(() => {
+    let result = bookmarks;
+
+
+
+    if (searchQuery) {
+      result = result?.filter((bookmark) =>
+        bookmark.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        bookmark.url.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+
+    const pinned = result?.filter((bookmark) => bookmark.is_pinned);
+    const unpinned = result?.filter((bookmark) => !bookmark.is_pinned);
+
+    return [...pinned!, ...unpinned!]
+
+    unpinned.sort((a, b) => {
+      if (sortBy === "recent") {
+        return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+      }
+      if (sortBy === "lastVisited") {
+        return new Date(b.last_visited).getTime() - new Date(a.last_visited).getTime();
+      }
+      if (sortBy === "mostVisited") {
+        return b.visit_count - a.visit_count;
+      }
+      return 0;
+
+    })
+
+
+  }, [bookmarks, selectedId, searchQuery, sortBy]);
 
   return (
     <>
-      {bookmarks?.map((bookmark) => (
+      {visibleBookmarks?.map((bookmark) => (
         <BookmarkCard key={bookmark.id} bookmark={bookmark} />
       ))}
     </>
